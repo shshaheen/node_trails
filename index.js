a0334f7ee3468ae85fb187f3c78f810e0c6bd73c
shshaheen <kolimishaheen9885@gmail.com>
2025-03-14 21:06:48 +0530

    Add nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const port =  3000;
 
 app.get('/', (req, res) =>{
-    res.send('Hello Developer!');  // Send a response with the message "Hello World!" when the root URL is accessed
+    res.send('Hello Developer');  // Send a response with the message "Hello World!" when the root URL is accessed
 }
 );
 app.listen(port,()=>{
