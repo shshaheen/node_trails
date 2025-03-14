caec017588462890f1563cb0dbe742ffe38604ea
shshaheen <kolimishaheen9885@gmail.com>
2025-03-14 20:49:26 +0530

    Basic set-up done!

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const port =  3000;
+
+app.get('/', (req, res) =>{
+    res.send('Hello Developer!');  // Send a response with the message "Hello World!" when the root URL is accessed
+}
+);
+app.listen(port,()=>{
+    console.log(`Server listenning on port http://localhost:${port}`);  // Log message when server is running
+});
